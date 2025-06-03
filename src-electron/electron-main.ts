90b1529826efc59bee45f77f44450742b9d006c2
UserId56 <zaregay_menay@mail.ru>
2025-06-03 13:31:19 +0700

    Инициализация приложения

---
+++ src-electron/electron-main.ts
@@ -0,0 +1,96 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import os from 'os';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+// needed in case process is undefined under Linux
+const platform = process.platform || os.platform();
+
+const currentDir = fileURLToPath(new URL('.', import.meta.url));
+
+let mainWindow: BrowserWindow | undefined;
+
+const windowStateFile = path.join(app.getPath('userData'), 'window-state.json');
+
+function loadWindowState() {
+  try {
+    return JSON.parse(fs.readFileSync(windowStateFile, 'utf-8'));
+  } catch {
+    return { width: 1000, height: 600, x: undefined, y: undefined };
+  }
+}
+
+function saveWindowState(window: BrowserWindow) {
+  const bounds = window.getBounds();
+  fs.writeFileSync(windowStateFile, JSON.stringify(bounds));
+}
+
+async function createWindow() {
+  const windowState = loadWindowState();
+
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    icon: path.resolve(currentDir, 'icons/icon.png'), // tray icon
+    width: windowState.width,
+    height: windowState.height,
+    x: windowState.x,
+    y: windowState.y,
+    fullscreen: false,
+    useContentSize: true,
+    webPreferences: {
+      contextIsolation: true,
+      // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
+      preload: path.resolve(
+        currentDir,
+        path.join(
+          process.env.QUASAR_ELECTRON_PRELOAD_FOLDER,
+          'electron-preload' + process.env.QUASAR_ELECTRON_PRELOAD_EXTENSION,
+        ),
+      ),
+    },
+  });
+
+  // Показать скрыть верхний навбар
+  mainWindow.setMenuBarVisibility(true);
+
+  if (process.env.DEV) {
+    await mainWindow.loadURL(process.env.APP_URL);
+  } else {
+    await mainWindow.loadFile('index.html');
+  }
+
+  if (process.env.DEBUGGING) {
+    // if on DEV or Production with debug enabled
+    mainWindow.webContents.openDevTools();
+  } else {
+    // we're on production; no access to devtools pls
+    mainWindow.webContents.on('devtools-opened', () => {
+      mainWindow?.webContents.closeDevTools();
+    });
+  }
+
+  mainWindow.on('close', () => {
+    saveWindowState(mainWindow!);
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = undefined;
+  });
+}
+
+void app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === undefined) {
+    void createWindow();
+  }
+});
